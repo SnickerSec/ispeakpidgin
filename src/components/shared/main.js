46f864b5cea42fe3c59a46936c6afcb91eb875a6
cwilliams-security <cwilliams@intellum.com>
2025-09-22 11:25:51 -1000

    fix: Simplify auto-translate label in translator functionality

--- src/components/shared/main.js
+++ src/components/shared/main.js
@@ -188,7 +188,7 @@ function initTranslator() {
                         <div class="block bg-green-500 w-14 h-8 rounded-full"></div>
                         <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition transform translate-x-6"></div>
                     </div>
-                    <span class="ml-3 text-gray-700 font-medium">Auto-translate (Always On)</span>
+                    <span class="ml-3 text-gray-700 font-medium">Auto-translate</span>
                 </div>
             `;
 
